b988702f119d01c0f021db6772baf322563b1c55
David Sandoval <jsandoval-2022231@kinal.edu.gt>
2024-02-14 04:11:42 -0600

    added connection server

---
+++ models/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Server running and listening at the port', this.port);
+        });
+    }
+}
+
+module.exports = Server;
