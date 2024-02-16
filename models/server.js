97d7d8806e8e9df313e47008ef924b4a8c279b83
David Sandoval <jsandoval-2022231@kinal.edu.gt>
2024-02-16 17:53:47 -0600

    added nothing

--- models/server.js
+++ models/server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../db/config');
 
 class Server{
+
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
+        this.userPath = '/api/user';   
 
+        this.connectDB();
+
+        this.middlewares();
+        
+        this.routes();
+    }
+
+    async connectDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes(){
+        this.app.use(this.userPath, require('../routes/user.routes'));
     }
 
     listen(){
